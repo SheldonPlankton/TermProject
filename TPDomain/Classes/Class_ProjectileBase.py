b887e286aaeb9ddf48597613438162d779f1623e
SheldonPlankton <tipping.evan@gmail.com>
2018-11-17 15:04:35 -0500

    More header documentation

--- TPDomain/Classes/Class_ProjectileBase.py
+++ TPDomain/Classes/Class_ProjectileBase.py
@@ -10,7 +10,7 @@
 # Class: Projectile Base
 # Created 11/15/2018
 
-# Version 0.1
+# Version 0.2
 
 # Planned features / updates:
 #   o Main feature description
@@ -20,7 +20,9 @@
 # Changelog:
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# No changes yet!
+# Updated to v0.2 on 11/17/2018
+# Changes:
+#   o Added projectile update method
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Imports:
@@ -34,5 +36,11 @@ from Classes.Class_Entity import Entity
 
 class ProjectileBase(Entity):
 
-    def __init__(self, xArg, yArg, spdArg, dirArg):
+    def __init__(self, xArg, yArg, spdArg, dirArg, lifeArg):
         super().__init__(xArg, yArg, spdArg, dirArg)
+        self.life = lifeArg
+
+    def update(self):
+        self.lifeArg -= 1
+        if not lifeArg:
+            return True
